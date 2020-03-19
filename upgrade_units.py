a7a6381c3324874aff0eb6303d5f21ad21096fe1
carolineheijmans <62341105+carolineheijmans@users.noreply.github.com>
2020-03-19 17:55:32 +0100

    Update upgrade_units.py

--- upgrade_units.py
+++ upgrade_units.py
@@ -22,7 +22,7 @@ def upgrade_unit(type_of_improvement):
     if type_of_improvement == regeneration :
 
         # Check conditions
-        if condition_upgrade_regeneration() == True :
+        if condition_upgrade() == True :
 
             # Upgrade regeneration rate of the hub
             info_player_1['hub']['regeneration_rate'] += 5
@@ -36,12 +36,12 @@ def upgrade_unit(type_of_improvement):
     elif type_of_improvement == storage :
 
         # Check conditions
-        if condition_upgrade_storage() == True :
+        if condition_upgrade() == True :
 
             # Upgrade storage of tankers
             for tanker in info_player_1['tankers'] :
 
-                info_player_1['tankers'][tankers]['capacity'] += 100
+                info_player_1['tankers'][tanker]['capacity'] += 100
             
             # Payement
             info_player_1['hub']['energy'] -= 600
@@ -53,7 +53,7 @@ def upgrade_unit(type_of_improvement):
     elif type_of_improvement == range :
 
         # Check conditions
-        if condition_upgrade_range() == True :
+        if condition_upgrade() == True :
 
             # Upgrade shooting range of cruisers
             for cruiser in info_player_1['cruisers'] :
@@ -70,7 +70,7 @@ def upgrade_unit(type_of_improvement):
     elif type_of_improvement == move :
 
         # Check conditions
-        if condition_upgrade_move() == True :
+        if condition_upgrade() == True :
 
             # Diminuates move price of cruisers
             for cruiser in info_player_1['cruisers'] :
@@ -88,16 +88,75 @@ def upgrade_unit(type_of_improvement):
 
     return info_player_1
 
-def condition_upgrade(hub):
+def condition_upgrade(type_of_improvement):
     """
     This function verify if the energy in the hub is enough for actions.
+    
     Parameter
-    hub: what hub is selected
+    ----------
+    type_of_improvement: what kind of upgrade the player wants make (str)
  
-    return:
-    energy_hub: how many energy there is in a hub
+    Return:
+    condition_upgrade: if conditions are okay or not (bool)
  
     Version:
-    Specification :Maxence Weyns (v.1 23/02/20)
+    Specification : Maxence Weyns (v.1 23/02/20)
+    Specification : Caroline Heijmans (v.2 19/03/2020)
+    Implementation : Caroline Heijmans (v.1 19/03/2020)
     """
+    if type_of_improvement == regeneration:
+        
+        if info_player_1['hub']['regeneration_rate'] == 50:
+            print('you have the maximal regeneration rate')
+            return False
+        else:
+            if calcul_energy >= 750 :
+                return True
+            else:
+                print('you dont have enough money for improve regeneration')
+                return False
+           
+       
+    elif type_of_improvement == storage:
+        
+        for tanker in info_player_1['tankers'] :
+            if info_player_1['tankers'][tanker]['capacity'] == 1200:
+                print ('you have the maximal capacity for your tankers')
+                return False
+            else:
+            if calcul_energy >= 600 :
+                return True
+            else:
+                print('you dont have enough money for improve storage')
+                return False
+    
+    elif type_of_improvement == range :
+        
+        for cruiser in info_player_1['cruisers'] :
+            if info_player_1['cruisers'][cruiser]['shooting_range'] == 5:
+                print ('you have the maximal range for your cruisers')
+                return False
+            else:
+            if calcul_energy >= 400 :
+                return True
+            else:
+                print('you dont have enough money to improve range')
+                return False
+            
+    elif type_of_improvement == move: 
+        
+        for cruiser in info_player_1['cruisers'] :
+            if info_player_1['cruisers'][cruiser]['move_price'] == 5 :
+                print ('you have the minimal moveprice for your cruisers')
+                return False
+            else:
+            if calcul_energy >= 500 :
+                return True
+            else:
+                print('you dont have enough money to diminuate move price')
+                return False
+        
+        
+  
+       
     
